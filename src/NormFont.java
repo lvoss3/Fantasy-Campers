64b478722f71860b71ced4a1c985a4e94b827cf2
Logan Voss <lvoss3@uis.edu>
2019-04-28 20:46:45 -0500

    Font test 3

--- src/NormFont.java
+++ src/NormFont.java
@@ -5,16 +5,16 @@ import java.io.IOException;
 
 public class NormFont {
 	public NormFont() {
-		/*GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+		GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
         try {
             ge.registerFont(Font.createFont(Font.TRUETYPE_FONT,
-                    getClass().getClassLoader().getResourceAsStream("/retro_computer_personal_use.ttf")));
+                    getClass().getClassLoader().getResourceAsStream("retro_computer_personal_use.ttf")));
         } catch (FontFormatException e1) {
             // TODO Auto-generated catch block
             e1.printStackTrace();
         } catch (IOException e1) {
             // TODO Auto-generated catch block
             e1.printStackTrace();
-        }*/
+        }
 	}
 }
